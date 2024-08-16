b5f844b377591d368b2bc138b7526bb2b56c802f
Pierre Paquin <p.paquin1016@laposte.net>
2024-08-16 08:12:07 +0200

    chore: modify command

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use ssh2::Session;
 use std::io::Read;
 use std::net::TcpStream;
+use std::ops::Add;
 
 fn main() {
 
@@ -10,6 +11,9 @@ fn main() {
     let user = std::env::var("USER_SSH").unwrap();
     let pwd = std::env::var("PWD_SSH").unwrap();
     let host = std::env::var("HOST_SSH").unwrap();
+    let special_chars = vec!['&', '$', '|'];
+    let escape_pwd = pwd.escape(special_chars.clone());
+
 
 
     let tcp = TcpStream::connect(host).unwrap();
@@ -19,10 +23,28 @@ fn main() {
     sess.userauth_password(&user, &pwd).unwrap();
 
     let mut channel = sess.channel_session().unwrap();
-    channel.exec(&format!("echo {pwd} | sudo -S docker volume ls")).unwrap();
-    // channel.exec(&format!("{pwd}")).unwrap();
+    channel.exec(&format!("echo {} | sudo -S docker volume ls", escape_pwd)).unwrap();
     let mut s = String::new();
     channel.read_to_string(&mut s).unwrap();
     println!("message :");
     println!("{s}");
+}
+
+impl CanEscape for String {}
+
+trait CanEscape {
+    fn escape(&self, chars: Vec<char>) -> String
+    where Self: ToString
+    {
+        self
+            .to_string()
+            .chars()
+            .fold("".to_string(), |acc, current| {
+                if chars.contains(&current) {
+                    acc.add(format!("\\{current}").as_str())
+                } else {
+                    acc.add(&current.to_string())
+                }
+            })
+    }
 }

0e53209c14f1bcfaf47b7856f31c56627883a91f
Pierre Paquin <p.paquin1016@laposte.net>
2024-08-16 08:05:43 +0200

    chore: change cmd

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 // use ssh::TerminalSize;
 // use ssh2::Session;
 
+use std::ops::Add;
 use ssh;
 
 fn main() {
@@ -15,6 +16,8 @@ fn main() {
     let user = std::env::var("USER_SSH").unwrap();
     let pwd = std::env::var("PWD_SSH").unwrap();
     let host = std::env::var("HOST_SSH").unwrap();
+    let special_chars = vec!['&', '$', '|'];
+    let escape_pwd = pwd.escape(special_chars.clone());
 
     let mut session = ssh::create_session()
         .username(&user)
@@ -23,24 +26,36 @@ fn main() {
         .connect(host)
         .unwrap();
 
-    // let utf8_ls = session.run_local()
-    //     .open_exec()
-    //     .unwrap()
-    //     // .send_command("docker volume -ls")
-    //     .send_command("ls")
-    //     .unwrap();
-
 
     let utf8_ls = session.run_local()
         .open_channel()
         .unwrap()
         .exec()
         .unwrap()
-        .send_command("ls")
+        .send_command(&format!("echo {} | sudo -S docker container ls", escape_pwd))
         .unwrap()
         .to_vec();
 
     let chaine = std::str::from_utf8(&utf8_ls).unwrap();
     //
     println!("{}", chaine);
+}
+
+impl CanEscape for String {}
+
+trait CanEscape {
+    fn escape(&self, chars: Vec<char>) -> String
+    where Self: ToString
+    {
+        self
+            .to_string()
+            .chars()
+            .fold("".to_string(), |acc, current| {
+                if chars.contains(&current) {
+                    acc.add(&format!("\\{current}"))
+                } else {
+                    acc.add(&current.to_string())
+                }
+            })
+    }
 }
